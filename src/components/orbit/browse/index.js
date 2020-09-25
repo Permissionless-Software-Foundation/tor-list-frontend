3a1adeec203f0f1d3225e1a393bbf8e0418107dd
MezzMar <gary.n.1999@gmail.com>
2020-09-25 14:20:18 +0000

    Fixed orbit entries listing

--- src/components/orbit/browse/index.js
+++ src/components/orbit/browse/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import './browse.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Row, Col, Box, Content, SimpleTable } from 'adminlte-2-react'
+import { Row, Col, Box, Content } from 'adminlte-2-react'
 import axios from 'axios'
 
 const SERVER = process.env.GATSBY_API_URL
 
 let _this
 class Browse extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super()
     _this = this
 
     this.state = {
@@ -17,7 +17,56 @@ class Browse extends React.Component {
     }
   }
 
-  render () {
+  componentDidMount() {
+    _this.fetchDbEntries()
+  }
+
+  async fetchDbEntries() {
+    try {
+      const res = await axios({
+        method: 'get',
+        // url: 'https://tor-list-api.fullstack.cash/orbitdb'
+        url: `${SERVER}/orbitdb`
+      })
+
+      _this.setState({ entries: res.data.entries })
+    } catch (error) {
+      console.log(error)
+      _this.setState({ entries: [] })
+    }
+  }
+
+  ContentTable() {
+    if (!_this.state.entries) {
+      return <Box className='p-5 no-shadow border-none' loaded={false} />
+    }
+    if (!_this.state.entries.length) {
+      return (
+        <div className='p-5 text-center'>
+          <p>There are no entries to show</p>
+        </div>
+      )
+    }
+
+    const rows = _this.state.entries.map((item) => {
+      return (
+        <tr key={item._id}>
+          <td>
+            <p><strong>{item.entry}</strong></p>
+            <p>{item.description.substring(0, 80)}</p>
+          </td>
+        </tr>
+      )
+    });
+
+    return (
+      <table className="table">
+        { rows }
+      </table>
+    )
+  }
+
+  render() {
     return (
       <Content browserTitle='Browse - Tor list'>
         <Row>
@@ -48,40 +97,6 @@ class Browse extends React.Component {
       </Content>
     )
   }
-
-  componentDidMount () {
-    _this.fetchDbEntries()
-  }
-
-  ContentTable () {
-    if (!_this.state.entries) {
-      return <Box className='p-5 no-shadow border-none' loaded={false} />
-    }
-    if (!_this.state.entries.length) {
-      return (
-        <div className='p-5 text-center'>
-          <p>There are not entries to show</p>
-        </div>
-      )
-    }
-    return <SimpleTable data={_this.state.entries} columns={columns} />
-  }
-
-  async fetchDbEntries () {
-    try {
-      const res = await axios({
-        method: 'get',
-        // url: 'https://tor-list-api.fullstack.cash/orbitdb/entries'
-        url: `${SERVER}/orbitdb/entries`
-      })
-      _this.setState({ entries: res.data.entries })
-    } catch (error) {
-      console.log(error)
-      _this.setState({ entries: [] })
-    }
-  }
 }
-
-const columns = [{ data: 'message' }]
 
 export default Browse
