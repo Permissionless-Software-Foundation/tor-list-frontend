1bb83ea05aab32453918ad0225e454e9149f3e15
Chris Troutner <chris.troutner@gmail.com>
2020-10-11 14:33:28 -0700

    fix(menu): Fixing menu to reflect changes in gatsby-ipfs-web-wallet v1.9.23

--- src/gatsby-ipfs-web-wallet/components/menu-components.js
+++ src/gatsby-ipfs-web-wallet/components/menu-components.js
@@ -9,21 +9,53 @@
 import React from 'react'
 import { Sidebar } from 'adminlte-2-react'
 
+import Wallet from 'gatsby-ipfs-web-wallet/src/components/admin-lte/wallet'
+import Tokens from 'gatsby-ipfs-web-wallet/src/components/admin-lte/tokens'
+import Configure from 'gatsby-ipfs-web-wallet/src/components/admin-lte/configure'
+import SendReceive from 'gatsby-ipfs-web-wallet/src/components/admin-lte/send-receive'
+
 import Browse from '../../components/orbit/browse'
 import Write from '../../components/orbit/write'
 
 const { Item } = Sidebar
 
-const MenuComponents = [
+const MenuComponents = props => [
   {
     key: 'Browse',
-    component: <Browse key='browse-component' />,
-    menuItem: <Item icon='fas-search' key='Browse' text='Browse' />
+    component: <Browse key="browse-component" />,
+    menuItem: <Item icon="fas-search" key="Browse" text="Browse" />,
+    active: true
   },
   {
     key: 'Write',
-    component: <Write key='write-component' />,
-    menuItem: <Item icon='fas-plus-square' key='Write Component' text='Write' />
+    component: <Write key="write-component" />,
+    menuItem: <Item icon="fas-plus-square" key="Write Component" text="Write" />
+  },
+  {
+    key: 'Tokens',
+    component: <Tokens key="Tokens" {...props} />,
+    menuItem: <Item icon="fas-coins" key="Tokens" text="Tokens" />
+  },
+  {
+    key: 'Send/Receive BCH',
+    component: <SendReceive key="Send/Receive BCH" {...props} />,
+    menuItem: (
+      <Item
+        icon="fa-exchange-alt"
+        key="Send/Receive BCH"
+        text="Send/Receive BCH"
+      />
+    )
+  },
+  {
+    key: 'Wallet',
+    component: <Wallet key="Wallet" {...props} />,
+    menuItem: <Item icon="fa-wallet" key="Wallet" text="Wallet" />
+  },
+  {
+    key: 'Configure',
+    component: <Configure key="Configure" {...props} />,
+    menuItem: <Item icon="fas-cog" key="Configure" text="Configure" />
   }
 ]
 
